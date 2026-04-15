21d6081208ebfbaf8e09cd6e1f9750eeb95d694b
Alexis <agrischin605@mail.com>
2026-04-15 21:42:29 +0300

    пора начинать думать

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.time.LocalTime;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.printf("Main: started at %d:%d:%d", LocalTime.now().getHour(), LocalTime.now().getMinute(), LocalTime.now().getSecond());
         TimeSlotDialog dialog = TimeSlotDialog.getInstance();
     }
